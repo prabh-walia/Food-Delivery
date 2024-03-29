3b237075553a958a1b93d5bfbba8f4873dbe87e0
prabh <prabhdeeps6911@gmail.com>
2024-03-29 17:49:25 +0530

    first commit

---
+++ dist/index.c36f364e.js
@@ -0,0 +1,17 @@
+const element = React.createElement("h1", {
+    id: "header"
+}, "Namaste React");
+const element2 = React.createElement("h2", {
+    id: "header"
+}, "Namaste React 2");
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    element,
+    element2
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(parent);
+root.render(parent);
+
+//# sourceMappingURL=index.c36f364e.js.map
